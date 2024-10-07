7fb7afe4db6ee894d54327766e1d404bde325708
Eber_Lab <eberto481@gmail.com>
2024-10-07 16:47:20 -0600

    Add practica_6_pruebas unitarias

--- PracticasNodeJs/Practica_6_pruebas_unitarias/webapp/dist/readHandler.js
+++ PracticasNodeJs/Practica_6_pruebas_unitarias/webapp/dist/readHandler.js
@@ -1,11 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.readHandler = void 0;
-const readHandler = (req, res) => {
-    // res.json({
-    //     message: "Hello, World"
-    // });
-    res.cookie("sessionID", "mysecretcode");
-    req.pipe(res);
+const promises_1 = require("fs/promises");
+// export const readHandler = (req: Request, res: Response) => {
+//     res.cookie("sessionID", "mysecretcode");
+//     req.pipe(res);
+// }
+// export const readHandler = (req:Request, res:Response) => {
+//     readFile("data.json", (err, data) => {
+//         if(err != null){
+//             res.writeHead(500, err.message);
+//         }
+//         else{
+//             res.setHeader("Content-Type", "application/json")
+//             res.write(data);
+//         }
+//         res.end();
+//     })
+// }
+const readHandler = async (req, res) => {
+    try {
+        res.setHeader("Content-Type", "application/json");
+        res.write(await (0, promises_1.readFile)("data.json"));
+    }
+    catch (err) {
+        res.writeHead(500);
+    }
+    res.end();
 };
 exports.readHandler = readHandler;
---
+++ PracticasNodeJs/Practica_6_pruebas_unitarias/webapp/dist/readHandler.test.js
@@ -0,0 +1,154 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//La funcionalidad de prueba se proporciona en el módulo node:test,
+const node_test_1 = require("node:test");
+const readHandler_1 = require("./readHandler");
+const assert_1 = require("assert");
+const promises_1 = __importDefault(require("fs/promises"));
+const fs_1 = require("fs");
+//la función más importante es test, que se utiliza para definir una prueba unitaria
+//acepta un nombre para la prueba y una función, que se ejecuta para realizar la prueba.
+// test("my new test name", () =>{
+//     //do nothing -test will pass
+// })
+// test("my new test name", () => {
+//     throw new Error("something went wrong");
+// })
+// test("readHandler tests", (testCtx) => {
+//     //Arrange -set up the test
+//     const req = {
+//         pipe: testCtx.mock.fn()
+//     };
+//     const res = {
+//         cookie: testCtx.mock.fn()
+//     };
+//     //TO DO -Act -perform the test
+//     //TO DO -Assert -verify the results
+// })
+// test("readHandler tests", (testCtx) => {
+//     //Arrange -set up the test
+//     const req = {
+//         pipe: testCtx.mock.fn()
+//     }
+//     const res = {
+//         cookie: testCtx.mock.fn()
+//     }
+//     //Act - perform the test
+//     readHandler(req, res);
+//     //Assert - verify the results
+//     if(req.pipe.mock.callCount() !== 1 || req.pipe.mock.calls[0].arguments[0] !== res){
+//         throw new Error("Request not piped");
+//     }
+//     if(res.cookie.mock.callCount() === 1){
+//         const [name, val] = res.cookie.mock.calls[0].arguments;
+//         if(name !== "sessionID" || val !== "mysecretecode"){
+//             throw new Error("Cookie not set correctly");
+//         }
+//     }
+//     else{
+//         throw new Error("cookie method not called once");
+//     }
+// })
+// test("readHandler tests", (testCtx) => {
+//     const req = {
+//         pipe: testCtx.mock.fn()
+//     }
+//     const res = {
+//         cookie: testCtx.mock.fn()
+//     }
+//     //Act - perform the test
+//     readHandler(req, res);
+//     //Assert -verify the results
+//     //El método equal se utiliza para realizar una serie de comparaciones y
+//     //arrojará un error que hará que la prueba falle si los valores no coinciden.
+//     equal(req.pipe.mock.callCount(), 1);
+//     equal(req.pipe.mock.calls[0].arguments[0], res);
+//     equal(res.cookie.mock.callCount(), 1);
+//     equal(res.cookie.mock.calls[0].arguments[0], "sessionID");
+//     equal(res.cookie.mock.calls[0].arguments[1], "mysecretcode");
+// })
+// test("readHandler tests", async(testCtx) =>{
+//     //Arrange - set up the test
+//     const data = "json-data";
+//     //simular la función readFile en el módulo fs, lo que se hace con esta declaración:
+//     //el método llamado method simula (mocks) un método en un objeto.
+//     testCtx.mock.method(fs, "readFile", (file, cb) => cb (undefined, data));
+//     const req = {};
+//     const res = {
+//         setHeader: testCtx.mock.fn(),
+//         write: testCtx.mock.fn(),
+//         end: testCtx.mock.fn()
+//     };
+//     //Act - perform the test
+//     await readHandler(req, res);
+//     //Assert - verify the results
+//     equal(res.setHeader.mock.calls[0].arguments[0], "Content-Type");
+//     equal(res.setHeader.mock.calls[0].arguments[1], "application/json");
+//     equal(res.write.mock.calls[0].arguments[0], data);
+//     equal(res.end.mock.callCount(), 1);
+// })
+// Prueba de una promesa en el archivo readHandler
+//La prueba simulada es una función asíncrona que produce 
+//los datos de prueba cuando se resuelve. El resto de la prueba unitaria no se modifica.
+// test("readHandler tests", async(testCtx) => {
+//     //Arrange -set up the test
+//     const data = "json-data";
+//     testCtx.mock.method(fs, "readFile", async() => data);
+//     const req = {};
+//     const res = {
+//         setHeader: testCtx.mock.fn(),
+//         write: testCtx.mock.fn(),
+//         end: testCtx.mock.fn()
+//     };
+//     //Act -perform the test
+//     await readHandler(req, res);
+//     //Assert -verify the results
+//     equal(res.setHeader.mock.calls[0].arguments[0], "Content-Type");
+//     equal(res.setHeader.mock.calls[0].arguments[1], "application/json");
+//     equal(res.write.mock.calls[0].arguments[0], data);
+//     equal(res.end.mock.callCount(), 1);
+// })
+//Prueba de multiples resultados en el archivo
+const createMockResponse = (testCtx) => ({
+    writeHead: testCtx.mock.fn(),
+    setHeader: testCtx.mock.fn(),
+    write: testCtx.mock.fn(),
+    end: testCtx.mock.fn()
+});
+(0, node_test_1.test)("readHandler tests", async (testCtx) => {
+    //Arrange -set up the test
+    const req = {};
+    // const res = {
+    //     setHeader: testCtx.mock.fn(),
+    //     write: testCtx.mock.fn(),
+    //     end: testCtx.mock.fn()
+    // };
+    //Test the successful outcome
+    await testCtx.test("Successfully reads file", async (innerCtx) => {
+        //Arrange -set up the test
+        const data = "json-data";
+        innerCtx.mock.method(promises_1.default, "readFile", async () => data);
+        const res = createMockResponse(innerCtx);
+        //Act - perform the test
+        await (0, readHandler_1.readHandler)(req, res);
+        //Assert -verify the results
+        (0, assert_1.equal)(res.setHeader.mock.calls[0].arguments[0], "Content-Type");
+        (0, assert_1.equal)(res.setHeader.mock.calls[0].arguments[1], "application/json");
+        (0, assert_1.equal)(res.write.mock.calls[0].arguments[0], data);
+        (0, assert_1.equal)(res.end.mock.callCount(), 1);
+    });
+    //Test the failure outcome
+    await testCtx.test("Handles error reading file", async (innerCtx) => {
+        //Arrange -set up the test
+        innerCtx.mock.method(promises_1.default, "readFile", () => Promise.reject("file error"));
+        const res = createMockResponse(innerCtx);
+        //Act -perform the test
+        await (0, readHandler_1.readHandler)(req, res);
+        //Assert -verify the results
+        (0, assert_1.equal)(res.writeHead.mock.calls[0].arguments[0], 500);
+        (0, assert_1.equal)(res.end.mock.callCount(), 1);
+    });
+});
