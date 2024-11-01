eea5d95554a43500a25ecac9c377bfc2048b1d88
new Eber's home <vazquezeber88@gmail.com>
2024-10-31 23:09:05 -0600

    Practica 10 terminada

---
+++ PracticasNodeJs/Practica_10_DB/part2app/src/server/data/orm_helpers.ts
@@ -0,0 +1,80 @@
+// El siguiente paso es indicarle a Sequelize cómo debe representarse 
+// cada propiedad definida por las clases del modelo en la base de datos
+import { DataTypes, Sequelize } from "sequelize";
+import { Calculation, Person, ResultModel } from "./orm_models";
+import { Result } from "./repository";
+
+const primaryKey = {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    }
+};
+
+export const initializeModels = (sequelize: Sequelize) => {
+    // el método init, que acepta un objeto cuyas propiedades corresponden
+    // a las definidas por la clase. A cada propiedad se le asigna un objeto 
+    // de configuración que le indica a Sequelize cómo representar los datos en la base de datos.
+    Person.init({
+        ...primaryKey,
+        name: { type: DataTypes.STRING }
+    }, { sequelize });
+
+
+    Calculation.init({
+        ...primaryKey, 
+        // se selecciona un valor de la clase DataTypes 
+        // para especificar el tipo de datos SQL que se 
+        // utilizará cuando se cree la base de datos
+        age: { type: DataTypes.INTEGER},
+        years: { type: DataTypes.INTEGER},
+        nextage: { type: DataTypes.INTEGER},
+    }, { sequelize });
+
+    ResultModel.init({
+        ...primaryKey,
+        // El segundo argumento aceptado por el método init se utiliza para configurar el modelo de datos general
+    },{sequelize})
+}
+
+// La clase base Model proporciona métodos para describir las relaciones entre las clases del modelo,
+export const defineRelationships = () => {
+    // la propiedad foreignKey se utiliza para especificar la llave foránea en la clase 
+    // ResultModel para las relaciones uno a uno con los tipos Person y Calculation.
+    ResultModel.belongsTo(Person, { foreignKey: "personId" });
+    ResultModel.belongsTo(Calculation, { foreignKey: "calculationId"});
+}
+
+export const addSeedData = async (sequelize: Sequelize) => {
+    // El método Sequelize.query acepta una cadena que contiene una declaración SQL
+    await sequelize.query(`
+        INSERT INTO Calculations
+        (id, age, years, nextage, createdAt, updatedAt) VALUES
+        (1, 35, 5, 40, date(), date()),
+        (2, 35, 10, 45, date(), date())`);
+    
+    // Sequelize agrega estas columnas para realizar un seguimiento de 
+    // cuándo se crean y modifican las filas de la tabla.
+    await sequelize.query(`
+        INSERT INTO People (id, name, createdAt, updatedAt) VALUES
+        (1, 'Alice', date(), date()), (2, "Bob", date(), date())`);
+
+
+    await sequelize.query(`
+        INSERT INTO ResultModels (calculationId, personId, createdAt, updatedAt) VALUES
+        (1, 1, date(), date()), (2, 2, date(), date()),
+        (2, 1, date(), date());`);
+}
+
+// función que transforma los objetos ResultModel proporcionados por 
+// el paquete ORM en objetos Result requeridos por la interfaz Repository.
+export const fromOrmModel = (model: ResultModel | null): Result => {
+    return {
+        id: model?.id || 0,
+        name: model?.Person?.name || "",
+        age: model?.Calculation?.age || 0,
+        years: model?.Calculation?.years || 0,
+        nextage: model?.Calculation?.age || 0
+    }
+}
---
+++ PracticasNodeJs/Practica_10_DB/part2app/src/server/data/sql_repository.ts
@@ -0,0 +1,69 @@
+// IMPLEMENTACION DE LA INTERFAZ REPOSITORY
+// SE CREA LA CLASE SQLREPOSITORY QUE IMPLEMENTA AL INTERFAZ REPOSITORY
+// LA CUAL TIENE METODOS PARA GUARDAR Y OBTENER DATOS
+// ESTA CLASE IMPLEMENTA ESTOS MEDODOS Y TIENE UN OBJETO DE TIPO 
+// BASE DE DATOS. 
+
+import { readFileSync } from "fs";
+import { Database } from "sqlite3";
+import { Repository, Result } from "./repository";
+import { queryAllSql, queryByNameSql, insertPerson, insertCalculation, insertResult } from "./sql_queries";
+import { TransactionHelper } from "./sql_helpers";
+
+export class SqlRepository implements Repository {
+    db: Database;
+
+    // aqui se prepara la base de datos
+    constructor(){
+        // se crea la base de datos a utlizar
+        this.db = new Database("age.db");
+        // se leen las instruccioens de este archivo para crear las tablas necesarias
+        this.db.exec(readFileSync("age.sql").toString(), err => {
+                if (err != undefined) throw err;
+            }
+        );
+    }
+
+    // La implementación del método saveResult ejecuta las tres sentencias SQL. Cada sentencia
+    // requiere un objeto independiente para sus parámetros de consulta porque SQLite produce
+    // un error si hay propiedades sin usar en el objeto de parámetros
+    async saveResult(r: Result): Promise<number> {
+        return await new TransactionHelper()
+        .add(insertPerson, {$name: r.name})
+        .add(insertCalculation, {
+            $age: r.age,
+            $years: r.years,
+            $nextage: r.nextage
+        })
+        .add(insertResult, {
+            $name: r.name,
+            $age: r.age,
+            $years: r.years,
+            $nextage: r.nextage
+        })
+        .run(this.db);
+    }
+
+    getAllResults($limit: number): Promise<Result[]> {
+        return this.executeQuery(queryAllSql, { $limit });
+    }
+
+    getResultsByName($name: string, $limit: number): Promise<Result[]> {
+        return this.executeQuery(queryByNameSql, { $name, $limit });
+    }
+
+    // El método executeQuery utiliza el método Database.all, que ejecuta una 
+    // consulta SQL y devuelve todas las filas que produce la base de datos.
+    executeQuery(sql: string, params: any): Promise<Result[]> {
+        return new Promise<Result[]>((resolve, reject) => {
+            this.db.all<Result>(sql, params, (err, rows) => {
+                if (err == undefined) {
+                    resolve(rows);
+                } else {
+                    reject(err);
+                }
+            })
+        });
+    }
+
+}
