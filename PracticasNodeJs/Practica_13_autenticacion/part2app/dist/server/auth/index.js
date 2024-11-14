5bb08e95a7641d5261d05e0efe8c92988473334c
new Eber's home <vazquezeber88@gmail.com>
2024-11-14 06:18:24 -0600

    Practica 13 avance

--- PracticasNodeJs/Practica_13_autenticacion/part2app/dist/server/auth/index.js
+++ PracticasNodeJs/Practica_13_autenticacion/part2app/dist/server/auth/index.js
@@ -5,12 +5,26 @@ const orm_authstore_1 = require("./orm_authstore");
 const store = new orm_authstore_1.OrmAuthStore();
 // Este archivo exporta una función llamada createAuth, que configura la autenticación para la aplicación.
 const createAuth = (app) => {
+    app.use((req, res, next) => {
+        const username = req.session.username;
+        if (username) {
+            req.authenticated = true;
+            req.user = { username };
+        }
+        else {
+            req.authenticated = false;
+        }
+        res.locals.user = req.user;
+        res.locals.autenticación = req.authenticated;
+        next();
+    });
     // El contenido de la plantilla de inicio de sesión se procesa cuando se envía una solicitud GET a /signin
     app.get("/signin", (req, res) => {
         const data = {
             username: req.query["username"],
             password: req.query["password"],
             failed: req.query["failed"] ? true : false,
+            signinpage: true
         };
         res.render("signin", data);
     });
@@ -22,11 +36,20 @@ const createAuth = (app) => {
         const password = req.body.password;
         const valid = await store.validateCredentials(username, password);
         if (valid) {
+            req.session.username = username;
             res.redirect("/");
         }
         else {
             res.redirect(`/signin?username=${username}&password=${password}&failed=1`);
         }
+    });
+    // permite que un usuario cierre la sesión de la aplicación destruyendo la sesión, 
+    // llamando al método destroy, que es una característica proporcionada por el 
+    // paquete express-session agregado al proyecto en un documento/práctica previa
+    app.post("/signout", async (req, res) => {
+        req.session.destroy(() => {
+            res.redirect("/");
+        });
     });
 };
 exports.createAuth = createAuth;
--- PracticasNodeJs/Practica_13_autenticacion/part2app/src/server/auth/index.ts
+++ PracticasNodeJs/Practica_13_autenticacion/part2app/src/server/auth/index.ts
@@ -4,14 +4,51 @@ import { OrmAuthStore } from "./orm_authstore";
 
 const store:AuthStore = new OrmAuthStore();
 
+type User = {username: string}
+
+// La primera declaración de declare extiende la interfaz SessionData 
+// para definir una propiedad de nombre de usuario de modo que la 
+// identidad de un usuario pueda asociarse con una sesión.
+declare module "express-session" {
+    interface SessionData { username: string }
+}
+
+// La segunda declaración de declare agrega propiedades de
+// usuario y autenticado a la interfaz Express Request, 
+// lo que permitirá que se proporcionen datos de usuario
+//  más complejos al resto de la aplicación
+declare global {
+    module Express {
+        interface Request {user: User, authenticated: boolean}
+    }
+}
+
 // Este archivo exporta una función llamada createAuth, que configura la autenticación para la aplicación.
 export const createAuth = (app: Express) => {
+
+    app.use((req, res, next) => {
+        const username = req.session.username;
+        if(username){
+            req.authenticated = true;
+            req.user = {username};
+        }
+        else{
+            req.authenticated = false
+        }
+        res.locals.user = req.user;
+        res.locals.autenticación = req.authenticated;
+        next();
+    })
+
+
+
     // El contenido de la plantilla de inicio de sesión se procesa cuando se envía una solicitud GET a /signin
     app.get("/signin", (req, res) => {
         const data = {
             username: req.query["username"],
             password: req.query["password"],
             failed: req.query["failed"] ? true: false,
+            signinpage: true
         }
         res.render("signin", data)
     })
@@ -25,10 +62,20 @@ export const createAuth = (app: Express) => {
         const valid = await store.validateCredentials(username, password);
         
         if(valid){
+            req.session.username = username;
             res.redirect("/");
         }
         else{
             res.redirect(`/signin?username=${username}&password=${password}&failed=1`);
         }
     });
+
+    // permite que un usuario cierre la sesión de la aplicación destruyendo la sesión, 
+    // llamando al método destroy, que es una característica proporcionada por el 
+    // paquete express-session agregado al proyecto en un documento/práctica previa
+    app.post("/signout", async (req, res)=> {
+        req.session.destroy(() => {
+            res.redirect("/");
+        })
+    })
 }
